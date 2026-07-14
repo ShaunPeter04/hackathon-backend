99b6352a969fbfa64a91463843c77da00e6c8298
ShaunPeter04 <shaunpeter04@gmail.com>
2026-07-14 18:30:55 +0530

    mongo connected

--- hackathon.js
+++ hackathon.js
@@ -6,6 +6,37 @@ const cors = require("cors")
 const app = express()
 app.use(cors())
 app.use(express.json())
+mongoose.connect("mongodb+srv://shaun:ares2008@cluster0.dsyjzb7.mongodb.net/?appName=Cluster0").then(
+
+    () => {
+        console.log("mongodb connected")
+    }
+
+).catch(
+    () => {
+        console.log(error)
+    }
+)
+
+const Team = mongoose.model("Teams", new mongoose.Schema(
+
+    {
+        teamId: String,
+        teamName: String,
+        teamLeaderName: String,
+        leaderEmail: String,
+        leaderPhone: String,
+        collegeName: String,
+        numberOfMembers: Number,
+        projectTitle: String,
+        problemStatementTrack: String,
+        technologyStack: String,
+        mentorName: String,
+        registrationDate: Date,
+        tableStationNumber: String
+    }
+
+))
 
 
 app.get("/test", (req, res) => {
